7f018fa655438142914e8d46d523d34badb9f44a
jj <jiangjun@guozi>
2017-12-26 14:17:00 +0800

    first commit

---
+++ src/main/java/com/gznb/mq/kafla/test/TestKafkaApplication.java
@@ -0,0 +1,19 @@
+package com.gznb.mq.kafla.test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+/**
+ * @author jiangjun
+ * @create 2017/11/23
+ */
+@SpringBootApplication(exclude = {HibernateJpaAutoConfiguration.class })
+public class TestKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestKafkaApplication.class,args);
+    }
+
+}
